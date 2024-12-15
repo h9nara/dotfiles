cda5b2b309f94ce388c2dad53eb6979cfb5e6ff2
Hendrik Nara <hdrkdev@gmail.com>
2024-12-14 18:07:26 -0800

    Add spacehsip prompt to zsh plugins.

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -3,3 +3,6 @@ source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # Zsh-vi-mode
 source ~/.zsh/plugins/zsh-vi-mode/zsh-vi-mode.plugin.zsh
+
+# Spaceship prompt
+source ~/.zsh/plugins/spaceship-prompt/spaceship.zsh
